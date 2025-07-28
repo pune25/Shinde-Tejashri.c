235e0cba14b2bfdff7a76473af0f5a871c6581c3
pune25 <shindetejashri38@gmail.com>
2025-07-28 18:26:24 +0530

    Add files via upload

---
+++ Shinde Tejashri.C/assingnment 10/strcmpQ7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <string.h>
+void main ()
+{
+	char str []="tejashri ";
+	char ptr[]="shinde";
+	
+   printf("	%d",strcmp(str,ptr));
+	
+}
---
+++ Shinde Tejashri.C/assingnment 10/strcpy2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <string.h>
+void main ()
+{
+	char str []="Tejashri ";
+	char ptr[]="Shinde";
+	
+	strcat(str,ptr);
+	puts(str);
+}
